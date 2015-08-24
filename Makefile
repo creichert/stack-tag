0c7df9e0150090a3ca293f3a388b5a91655f3cbb
Christopher Reichert <creichert07@gmail.com>
2015-08-24 11:46:46 -0500

    Implement new test-suite with placeholders

--- Makefile
+++ Makefile
@@ -1,3 +1,15 @@
 
-all:
+STKTAG=.stack-work/install/x86_64-linux/lts-3.0/7.10.2/bin/stack-tag
+
+.PHONY: all build test TAGS
+
+all: test build
+
+build:
 	stack build
+
+test:
+	stack test
+
+TAGS: build
+	$(STKTAG)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Hspec
+import Test.QuickCheck
+import Stack.Tag
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Stack.Tag" $ do
+    it "sanity checks" $
+        reverse [1,2,3,4] `shouldBe` [4,3,2,1]
+    it "sanity checks quickcheck" $ property $
+        \x -> (read . show) x == (x :: Int)
